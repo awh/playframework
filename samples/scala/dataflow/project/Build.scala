77106be30144997ee23e966b1b67399cd578352a
Adam Harrison <adam-github@antispin.org>
2013-09-07 18:08:19 +0100

    Add Akka DataFlow dependency and activate continuations plugin

--- samples/scala/dataflow/project/Build.scala
+++ samples/scala/dataflow/project/Build.scala
@@ -8,14 +8,14 @@ object ApplicationBuild extends Build {
   val appVersion      = "1.0-SNAPSHOT"
 
   val appDependencies = Seq(
-    // Add your project dependencies here,
-    jdbc,
-    anorm
+    "com.typesafe.akka" %% "akka-dataflow" % "2.1.0"
   )
 
 
   val main = play.Project(appName, appVersion, appDependencies).settings(
-    // Add your own project settings here      
-  )
+    autoCompilerPlugins := true,
+    libraryDependencies <+= scalaVersion { v =>
+      compilerPlugin("org.scala-lang.plugins" % "continuations" % v) },
+    scalacOptions += "-P:continuations:enable"  )
 
 }
